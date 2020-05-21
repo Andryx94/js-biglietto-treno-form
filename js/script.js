5da32c2648661c8ff23f7dbbf616e8a8d3d2dece
Andrea <andryx1994@gmail.com>
2020-05-21 17:30:01 +0200

    Versione 0.7
    Implementate tutte le sezioni tranne calcolo costo biglietto

--- js/script.js
+++ js/script.js
@@ -1,26 +1,40 @@
-var chilometri;
-document.getElementById('chilometri').innerHTML =  chilometri = Math.abs(parseInt(prompt("Dimmi i chilometri del tuo viaggio")));
-var anni;
-document.getElementById('anni').innerHTML =  anni = Math.abs(parseInt(prompt("Dimmi quanti anni hai")));
+//variabili generazione biglietto
+var nomeCognome = document.getElementById('nome-cognome');
+var chilometri = document.getElementById('chilometri');
+var eta = document.getElementById('eta');
 
-var prezzo = (chilometri * 0.21);
-var prezzoJunior = prezzo - (prezzo * 0.2); // applica lo sconto del 20% per i minorenni
-var prezzoSenior = prezzo - (prezzo * 0.4); // applica lo sconto del 40% per gli over 65
+// variabili bottoni
+var genera = document.getElementById('genera')
+var annulla = document.getElementById('annulla')
 
-if (isNaN(chilometri) || isNaN(anni)) {
-  alert("Inserisci correttamente i dati!")
+//variabile generazione report
+var report = document.getElementById('report')
+var reportNomeCognome = document.getElementById('report-nome-cognome');
+var reportEta = document.getElementById('report-eta');
+var reportCarrozza = document.getElementById('report-carrozza');
+var reportCp = document.getElementById('report-cp');
+var reportCosto = document.getElementById('report-costo');
+
+//variabili varie
+
+//attivazione bottone genera
+genera.addEventListener('click',
+function() {
+  reportNomeCognome.innerHTML= nomeCognome.value;
+  reportEta.innerHTML= eta.value;
+  reportCarrozza.innerHTML= "A" + Math.floor(Math.random() * 10 + 1);
+  reportCp.innerHTML= Math.floor(Math.random() * 100000);
+  report.className = report.classList + " nohide";
 }
+);
 
-else {
-  if (anni < 18) {
-    document.getElementById('prezzo').innerHTML =  prezzoJunior.toFixed(2);
-  }
-
-  else if (anni > 65){
-    document.getElementById('prezzo').innerHTML =  prezzoSenior.toFixed(2);
-  }
-
-  else {
-    document.getElementById('prezzo').innerHTML =  prezzo.toFixed(2);
-  }
+//attivazione bottone annulla
+annulla.addEventListener('click',
+function() {
+  reportNomeCognome.innerHTML= nomeCognome.value = "";
+  reportEta.innerHTML= eta.value = "";
+  reportCarrozza.innerHTML= "";
+  reportCp.innerHTML= "";
+  report.className = report.classList + "biglietto-report hide";
 }
+);
